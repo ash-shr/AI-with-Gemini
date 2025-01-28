fc642733787631c08ff0bc837920e1e6cb4063f8
Ashr005 <as2733@sussex.ac.uk>
2025-01-28 13:47:21 +0000

    Update ContentView.swift

--- AI with Gemini/ContentView.swift
+++ AI with Gemini/ContentView.swift
@@ -3,7 +3,10 @@ import SwiftUI
 import GoogleGenerativeAI
 
 struct ContentView: View {
-    
+    let model = GenerativeModel(name: "gemini-pro", apiKey: APIKey.default)
+    @State var userPrompt = ""
+    @State var response: LocalizedStringKey = "How can I help you today?"
+    @State var isLoading = false
     
     var body: some View {
         VStack {
